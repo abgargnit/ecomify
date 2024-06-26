3f059311c3222dc6e0d2ea139faa471c356c0dad
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-06-27 01:16:12 +0530

    add user routes user controller user login

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import productRoutes from './routes/productsRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 import ConnectDB from './config/db.js';
 import dotenv from 'dotenv';
 import { notFound,errorHandler } from './middlewares/errorMiddleware.js';
+
 
 
 dotenv.config();
@@ -11,12 +13,18 @@ ConnectDB() // connect to database....
 const port = process.env.PORT || 8000;
 const app = express();
 
+// Body parser middlewware...
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
+
 
 app.get('/',(req,res)=>{
     res.send("API is running...") // creating routes
 });
 
 app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
