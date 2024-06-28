207e4fd8c19fe43708653916586077fd5b8e7491
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-06-29 00:21:19 +0530

    add userApislice and authSlice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSLice } from "./slices/apiSlice";
+import { apiSLice } from "./slices/apiSlice.js";
 import  cartSliceReducer  from './slices/cartSlice.js'
+import authReducer from './slices/authSlice.js';
 
 
 const store = configureStore({
     reducer: {
         [apiSLice.reducerPath] : apiSLice.reducer,
         cart:cartSliceReducer,
+        auth:authReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSLice.middleware),
     devTools : true, // we can if we are only in development 
