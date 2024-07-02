b256787ec622b66a43de1fa1b2a199158965baaf
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-07-03 01:32:58 +0530

    add payment Screen

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -1,11 +1,11 @@
 import { Nav } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 
-const CheckoutSteps = (step1,step2,step3,step4) => {
+const CheckoutSteps = ({step1,step2,step3,step4}) => {
   return (
     <Nav className="justify-content-center mb-4">
         <Nav.Item>
-        {step1 ? (
+        { step1 ? (
             <LinkContainer to='/login'>
                 <Nav.Link>Sign In</Nav.Link>
             </LinkContainer>
