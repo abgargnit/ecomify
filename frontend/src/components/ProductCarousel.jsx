ccca2ed2ce48c503cbfa19ebb3605b2dbc047c80
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-10-07 15:32:26 +0530

    remove product carousel loader

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Carousel,Image } from 'react-bootstrap'
-import Loader from './loader.jsx'
 import Message from './message.jsx'
 import { useGetTopProductsQuery } from '../slices/productsApislice.js'
 
 
 const ProductCarousel = () => {
-    const {data:products,isLoading,error} = useGetTopProductsQuery();
-
-    return isLoading ? <Loader/> : error ? <Message>{error}</Message> : (
-        <Carousel pause='hover' className='bg-primary mb-4'>
-            {products.map(product=>(
-                <Carousel.Item key={product._id}>
-                    <Link to={`/products/${product._id}`}>
-                    < Image src={product.image} alt={product.name} fluid/>
-                    <Carousel.Caption className='carousel-caption'>
-                    <h2>{product.name} (${product.price})</h2>
-                    </Carousel.Caption>
-                    </Link>
-                </Carousel.Item>
-            ))}
-
-
-        </Carousel>
-    )
-}
+    const { data: products, error } = useGetTopProductsQuery();
+    return error ? (
+        <Message>{error}</Message>
+    ) : (
+        products && products.length > 0 && (
+            <Carousel pause='hover' className='bg-primary mb-4'>
+                {products.map(product => (
+                    <Carousel.Item key={product._id}>
+                        <Link to={`/products/${product._id}`}>
+                            <Image src={product.image} alt={product.name} fluid />
+                            <Carousel.Caption className='carousel-caption'>
+                                <h2>{product.name} (${product.price})</h2>
+                            </Carousel.Caption>
+                        </Link>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+        )
+    );
+};
 
 export default ProductCarousel
